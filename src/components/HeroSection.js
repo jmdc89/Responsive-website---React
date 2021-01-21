246c0171bff98a330eece7fee7cff0d72c147a8e
Jose Manuel <jmdcarq89@gmail.com>
2021-01-21 19:29:42 +0100

    Home v1

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-           <video src="/videos/video-2.mp4" autoPlay loop muted />
+           <video src="../videos/video-2.mp4" autoPlay loop muted />
            <h1>ADVENTURE AWAITS</h1>
            <p>What are you waiting for?</p>
            <div className="hero-btns">
@@ -16,6 +16,13 @@ function HeroSection() {
                 buttonSize='btn--large'
             >
                 GET STARTED
+            </Button>
+            <Button 
+                className='btns' 
+                buttonStyle='btn--primary'
+                buttonSize='btn--large'
+            >
+                WATCH TRAILER <i className='far fa-play-circle' />
             </Button>
            </div>
         </div>
